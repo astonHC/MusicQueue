f892130d243dc991d5fcd02f091cd4c246daf893
Harry Clark <139020900+astonHC@users.noreply.github.com>
2024-11-13 15:35:55 +0000

    Update Main.java

--- music/Main.java
+++ music/Main.java
@@ -5,9 +5,6 @@ public class Main
 {
     public final static void main(String[] args)
     {
-        while(true)
-        {
-            Music.ARGS(args);
-        }
+        Music.ARGS(args);
     }
 }
